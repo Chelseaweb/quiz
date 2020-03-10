e5e5ae7dfdbb6b78cc82cdaeb9c3364926549ccc
Chelseaweb <chelseamutizira@gmail.com>
2020-03-10 12:22:58 +0200

    changes to animation

--- landingpage.php
+++ landingpage.php
@@ -1,6 +1,26 @@
 <!Doctype html>
  <html lang="en">
 <head>
+  <style>
+    div {
+    border:black 2px solid ;
+      width:650px;
+      height:auto;
+      color:black;
+      text-align: center;
+      margin-top:30px;
+      margin-left:auto;
+      margin-right:auto;
+     background-color: cornsilk;
+    animation-name: example;
+    animation-duration:6s;
+  }
+  
+  @keyframes example {
+    from {background-color: brown ;}
+    to {background-color: burlywood;}
+  }
+  </style>
 <title>Quiz</title>
  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial scaleb=1.0">
