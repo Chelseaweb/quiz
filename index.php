8800a2b300a928c57cb0c2b17affa928560ab002
Chelseaweb <60740140+Chelseaweb@users.noreply.github.com>
2020-05-02 17:27:57 -0700

    Add files via upload
    changes made to file name, aesthetics and capital variables.

---
+++ index.php
@@ -0,0 +1,44 @@
+<!Doctype html>
+ <html lang="en">
+<head>
+  <style>
+    div {
+    border:black 2px solid ;
+      width:400px;
+      height:auto;
+      color:black;
+      text-align: center;
+      margin-top:30px;
+      margin-left:auto;
+      margin-right:auto;
+     background-color: cornsilk;
+    animation-name: example;
+    animation-duration:6s;
+  }
+  @keyframes example {
+    from {background-color: brown ;}
+    to {background-color: burlywood;}
+  }
+  </style>
+  
+<title>Quiz</title>
+ <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial scaleb=1.0">
+  <link rel="stylesheet" type="text/css" href="style.css">
+    <!--google fonts-->
+     <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+     
+</head>
+<body>
+      <div class="col-12">
+        <h1>Only for the soccer fans</h1>
+          <h2><p>How good are you on the field?</p></h2>
+            <br>
+              <form action="quiz.php">
+              <header>
+               <h3><button>START QUIZ</button></h3>
+                </form>
+     </div>
+     </div>
+    </body>
+    </html>
--- quiz.php
+++ quiz.php
@@ -1,16 +1,16 @@
 <!Doctype html>
 <html lang="en">
 <head>
-
  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial scaleb=1.0">
     <title>PHP Quiz</title>
      <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
-    <div class="container">
+
+<div class="container">
         <div class="row">
           <div class="col-12">
             <div class="soccer">
@@ -20,9 +20,9 @@
 
 <?php
 
-$Question = array(
+$question = array(
     1=> array(
-    'Question'=>'Which is the first African country to host the world cup?',
+    'Question'=>'1)Which is the first African country to host the world cup?',
     'Answers' => array(
     'A'=>"Senegal",
     'B'=>"Namibia",
@@ -33,7 +33,7 @@ $Question = array(
 ),
 
     2=> array(
-    'Question'=>'Which club does Cristiano Ronaldo play for?',
+    'Question'=>'2)Which club does Cristiano Ronaldo play for?',
     'Answers'=> array(
      'A'=>"Ajax",
      'B'=>"Juventus",
@@ -44,7 +44,7 @@ $Question = array(
     ),
       
     3=> array(
-    'Question' => 'Bend it like Beckham is a phrase popular for which England player?',
+    'Question' => '3)Bend it like Beckham is a phrase popular for which England player?',
      'Answers' =>  array(
     'A'=> "David Beckham",
     'B'=> "Gareth Bale",
@@ -55,7 +55,7 @@ $Question = array(
     ),
 
     4=> array(
-    'Question'=> 'Who won the last world cup?',
+    'Question'=> '4)Who won the last world cup?',
     'Answers' => array(
     'A'=> "Spain",
     'B'=> "Portugal",
@@ -65,7 +65,7 @@ $Question = array(
     'CorrectAnswer'=> 'D'
 ),
      5=> array(
-    'Question'=> 'Which club has won the most Uefa Trophies?',
+    'Question'=> '5)Which club has won the most Uefa Trophies?',
     'Answers'=> array(
     'A'=> "Inter Milan",
     'B'=> "Bayern Munich",
@@ -76,7 +76,7 @@ $Question = array(
 ),
 
      6=> array(
-   'Question'=>'How many African countries are allowed to play in the world cup ?',
+   'Question'=>'6)How many African countries are allowed to play in the world cup ?',
    'Answers'=> array(
     'A'=> "six",
     'B'=> "four",
@@ -87,7 +87,7 @@ $Question = array(
 ),
     
     7=> array(
-   'Question'=>'Which soccer player has won the most Ballandor?',
+   'Question'=>'7)Which soccer player has won the most Ballandor?',
    'Answers'=> array(
     'A'=> "Cristiano Ronaldo",
     'B'=> "Lionel Messi",
@@ -97,7 +97,7 @@ $Question = array(
    'CorrectAnswer'=> 'A'
 ),
     8=> array(
-   'Question'=>'Which sport is Brazil recognized by?',
+   'Question'=>'8)Which sport is Brazil recognized by?',
    'Answers' => array(
     'A'=> "Tennis",
     'B'=> "Golf",
@@ -108,7 +108,7 @@ $Question = array(
 ),
 
     9=> array(
-   'Question'=>'Which country has its national  team called Samba boys?',
+   'Question'=>'9)Which country has its national  team called Samba boys?',
    'Answers'=>array(
      'A'=> "Australia",
      'B'=> "America",
@@ -119,7 +119,7 @@ $Question = array(
 ),
     
     10=> array(
-   'Question'=>'Which english team has been banned from UEFA for two years?',
+   'Question'=>'10)Which english team has been banned from UEFA for two years?',
       'Answers' => array(
      'A'=> "Manchester City",
      'B'=> "Bournemouth",
@@ -130,7 +130,7 @@ $Question = array(
     ),
 
     11=> array(
-   'Question'=> 'Which former Chelsea player is now the head coach of Chelsea?',
+   'Question'=> '11)Which former Chelsea player is now the head coach of Chelsea?',
    'Answers'=> array(
      'A'=> "John Terry",
      'B'=> "Didier Drogba",
@@ -140,7 +140,7 @@ $Question = array(
     'CorrectAnswer'=> 'C'
 ),
     12=> array(
-   'Question'=>'Who is the highest paid soccer player of 2019?',
+   'Question'=>'12)Who is the highest paid soccer player of 2019?',
    'Answers'=> array(
       'A'=> "Sterling",
       'B'=> "Rashford",
@@ -150,7 +150,7 @@ $Question = array(
     'CorrectAnswer'=> 'D'
 ),
      13=> array(
-   'Question'=>'Old Trafford belongs to which club?',
+   'Question'=>'13)Old Trafford belongs to which club?',
    'Answers'=> array(
     'A'=>"Manchester United",
     'B'=>"Chelsea",
@@ -161,7 +161,7 @@ $Question = array(
 ),
     
     14=> array(
-    'Question'=>'Which club does Zinedine Zidane coach?',
+    'Question'=>'14)Which club does Zinedine Zidane coach?',
     'Answers'=> array(
       'A'=> "Atletico Madrid",
       'B'=> "Real Madrid",
@@ -171,7 +171,7 @@ $Question = array(
      'CorrectAnswer'=> 'B'
 ),
     15=>array(
-     'Question'=>'Who is the current Fifa President?',
+     'Question'=>'15)Who is the current Fifa President?',
      'Answers'=> array(
       'A'=> "Eden Hazard",
       'B'=> "Isco",
@@ -181,7 +181,7 @@ $Question = array(
      'Correct Answer'=> 'C'
 ),
      16=>array(
-     'Question'=> 'Who is famously known for the hand of God?',
+     'Question'=> '16)Who is famously known for the hand of God?',
      'Answers'=> array(
       'A'=> "Pepe",
       'B'=> "Ronaldhino",
@@ -192,7 +192,7 @@ $Question = array(
     ),
 
        17=>array(
-      'Question'=>'Who opened the 2010 world cup?',
+      'Question'=>'17)Who opened the 2010 world cup?',
       'Answers'=> array(
        'A'=> "Shakira and FreshlyGround",
        'B'=> "The Parlotones",
@@ -202,7 +202,7 @@ $Question = array(
         'CorrectAnswer'=> 'A'
     ),
        18=>array(
-       'Question'=>'Who hosted the last world cup?',
+       'Question'=>'18)Who hosted the last world cup?',
        'Answers'=> array(
        'A'=> "Greece",
        'B'=> "China",
@@ -213,7 +213,7 @@ $Question = array(
     ),
    
        19=>array(
-        'Question'=>'Who has the highest paid transfer?',
+        'Question'=>'19)Who has the highest paid transfer?',
         'Answers'=> array(
        'A'=> "Ozil",
        'B'=> "Giroud",
@@ -224,7 +224,7 @@ $Question = array(
     ),
        
       20=>array(
-      'Question'=>'Which Barcelona player used to be known for biting other players?',
+      'Question'=>'20)Which Barcelona player used to be known for biting other players?',
       'Answers'=> array(
        'A'=>"Rakatic",
        'B'=>"Antoine Griezmann",
@@ -233,65 +233,55 @@ $Question = array(
       ),
       'CorrectAnswer'=>'D'
       )
+    
+); 
 
-);
 
-//if (isset($_POST['answers'])){
-  //  $Answers = $_POST['answers']; // Get submitted answers.
+ //if (isset($_POST['answers'])){
+  // $answers = $_POST['answers']; // Get submitted answers.
 
     // Now this is fun, automated question checking! ;)
 
-   // foreach ($Question as $QuestionNo => $Value){
-        // Echo the question
-       // echo $Value['Question'].'<br />';
+    // foreach ($question as $questionNo => $value){
+       // Echo the question
+      //  echo $value['Question'].'<br />';
         
         
-        //if ($Answers[$QuestionNo] != $Value['CorrectAnswer']){
-         //    echo 'You answered: <span style="color: red;">'.$Value['Answers'][$Answers[$QuestionNo]].'</span><br>'; // Replace style with a class
-         //  echo 'Correct answer: <span style="color: green;">'.$Value['Answers'][$Value['CorrectAnswer']].'</span>';
+     //   if ($answers[$questionNo] != $value['CorrectAnswer']){
+          //  echo 'You answered: <span style="color: red;">'.$value['Answers'][$answers[$questionNo]].'</span><br>'; // Replace style with a class
+         //   echo 'Correct answer: <span style="color: green;">'.$value['Answers'][$value['CorrectAnswer']].'</span>';
        // } else {
-        //    echo 'Correct answer: <span style="color: green;">'.$Value['Answers'][$Answers[$QuestionNo]].'</span><br>'; // Replace style with a class
-           // echo 'You are correct: <span style="color: green;">'.$Value['Answers'][$Answers[$QuestionNo]].'</span>';   $counter++;               
+          //  echo 'Correct answer: <span style="color: green;">'.$value['Answers'][$answers[$questionNo]].'</span><br>'; // Replace style with a class
+           // echo 'You are correct: <span style="color: green;">'.$value['Answers'][$answers[$questionNo]].'</span>';$counter++;
+       // }                
+     // } 
+     //}  
+                                                                 
+                            
+      echo '<br /><hr>'; 
+                            
+                       if ($counter="");
+                         {
+                         ($counter++);
+                         $results = "Your score: $counter/20"; 
+                         }
+                        
+                        echo $results;                         
+                                        
+        
+         ?>                           
 
-      //  }
-          
-        echo '<br /><hr>'; 
-      
-                                if ($counter=="") 
-                                { 
-                                $counter='0';
-                                $results = "Your score: $counter/20"; 
-                                }
-                                else 
-                                { 
-                                $results = "Your score: $counter/20"; 
-                                }
-                             echo $results;
-
-                                 if ($answers<='10' || $answers >'10')
-                                 {  
-                                      $results="Perhaps you should do your history!";
-                                 }
-
-                                 else
-                                 {
-                                    $result="You deserve the golden boot!";
-                                 }
-                                 echo $results;
-                                                                
-                                 
-?>
     <form action="quiz.php" method="post" id="quiz">
-    <?php foreach ($Question as $QuestionNo => $Value) { ?>
+    <?php foreach ($question as $questionNo => $value) { ?>
 
-        <h3><?php echo $Value['Question']; ?></h3>
+        <h3><?php echo $value['Question']; ?></h3>
         <?php 
-            foreach ($Value['Answers'] as $Letter => $Answer){ 
-            $Label = 'question-'.$QuestionNo.'-answers-'.$Letter;
+            foreach ($value['Answers'] as $letter => $answer){ 
+            $label = 'question-'.$questionNo.'-answers-'.$letter;
         ?>
         <div>
-            <input type="radio" name="answers[<?php echo $QuestionNo; ?>]" id="<?php echo $Label; ?>" value="<?php echo $Letter; ?>" />
-            <label for="<?php echo $Label; ?>"><?php echo $Letter; ?>) <?php echo $Answer; ?> </label>
+            <input type="radio" name="answers[<?php echo $questionNo; ?>]" id="<?php echo $label; ?>" value="<?php echo $letter; ?>" />
+            <label for="<?php echo $label; ?>"><?php echo $letter; ?>) <?php echo $answer; ?> </label>
         </div>
         <?php } ?>
 
@@ -301,7 +291,18 @@ $Question = array(
 <?php 
 
 ?>
-</body>
-</html>
+<?php 
 
+?>
 
+</div>
+ </div>
+ </div>
+ </div>
+ 
+
+ </body>
+ </html>
+ 
+  
+  
